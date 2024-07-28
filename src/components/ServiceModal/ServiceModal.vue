e9d166c5268f8af88d4039b4905a3aabe5f6c8b1
jack <jack@gmail.com>
2024-07-28 23:40:31 +0800

    聊天快捷语设置

--- src/components/ServiceModal/ServiceModal.vue
+++ src/components/ServiceModal/ServiceModal.vue
@@ -3,7 +3,7 @@
   <n-modal to="body" v-model:show="isShow" :mask-closable="false" transform-origin="center">
     <div class="main">
       <h4 class="top_title">
-          <span>与阿铁的聊天 (官方客服)</span>
+          <span @click="showSetting">与阿铁的聊天 (官方客服)</span>
           <i>
             <iconpark-icon @click="isShow = false" icon-id="Group39368" color="#fff" size="1.2rem"></iconpark-icon>
           </i>
@@ -16,17 +16,20 @@
           <chatArea></chatArea>
         </div>
         </div>
+      <!-- 快捷语设置 -->
+      <shortcutSettings v-model:visible="visibleSetting"/>
     </div>
   </n-modal>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import {computed, ref} from 'vue';
 // import btn from './btn.vue';
 // import Common from '@/utils/common';
 // import { Net } from '@/net/Net';
 // import { NetPacket } from '@/netBase/NetPacket';
 import chatArea from './chatArea.vue';
+import shortcutSettings from './shortcutSettings.vue';
 // import { MessageEvent2 } from '@/net/MessageEvent2';
 // import { NetMsgType } from '@/netBase/NetMsgType';
 // import { Message } from '@/utils/discreteApi';
@@ -40,7 +43,7 @@ const props = defineProps({
   },
 });
 const emit = defineEmits(['update:visible']);
-
+const visibleSetting = ref(false) // 快捷语设置
 
 
 const isShow = computed({
@@ -51,6 +54,12 @@ const isShow = computed({
     emit('update:visible', value);
   },
 });
+
+// 打开快捷语设置
+const showSetting = () => {
+  visibleSetting.value = true
+}
+
 </script>
 <style lang="less" scoped>
 .main {
