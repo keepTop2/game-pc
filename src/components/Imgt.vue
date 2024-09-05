5de8819cd8bf1c464c1bd5342e658901b70e206e
lolo <cyan4game@gmail.com>
2024-09-05 10:57:40 +0800

    图片组件处理

---
+++ src/components/Imgt.vue
@@ -0,0 +1,15 @@
+<!-- 带时间戳的图片组件 -->
+<template>
+    <img :src="props.src + '?t=' + Date.now()" alt="img">
+</template>
+
+<script setup lang='ts' name="Img">
+
+const props: any = defineProps({
+    src: {
+        type: String,
+        default: ''
+    }
+})
+
+</script>
