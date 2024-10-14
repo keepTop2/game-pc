4eecf24cd389fe9ff0f3282d7ed4aa635b269034
Chris <chris.hong@aibit-tech.com>
2024-10-14 12:05:21 +0800

    赛事排行修改

--- src/views/newHome/homeOther.vue
+++ src/views/newHome/homeOther.vue
@@ -55,8 +55,8 @@
         </p>
         <div class="match_wrap">
           <Imgt class="match_img" :src="`/img/home/new_match.webp`" />
-          <n-carousel style="position: static;margin-left:35px;width:97.5%" :slides-per-view="7.2" :loop="false" draggable
-            show-arrow>
+          <n-carousel style="position: static;margin-left:35px;width:97.5%" :slides-per-view="7.2" :loop="false"
+            draggable show-arrow>
             <div v-for="i in 10" :key="i" :class="['match_list', { active_match: match_id == i }]"
               @click="matchClick(i)">
               <span>德州扑克</span>
@@ -75,7 +75,48 @@
       </div>
     </div>
     <div class="to_match">
-      <Imgt class="remark_img" :src="`/img/home/remark.webp`" />
+      <div class="card_des_wrap">
+        <div class="card_des">
+          <span class="title">赛事简介</span>
+          <div class="content">
+            欢迎参加本年度最激动人心的德州扑克锦标赛！无论您是经验丰富的扑克高手，还是刚刚入门的新手，这场比赛都将为您提供一个展示技巧和策略的绝佳平台。
+          </div>
+        </div>
+        <div class="card_des">
+          <span class="title">比赛详情</span>
+          <div class="card_des_date">
+            <span> •  日期：2024年7月20日</span>
+            <span> •  时间：下午2:00开始</span>
+            <span> •  报名费：₱5,000</span>
+          </div>
+        </div>
+        <div class="card_des">
+          <span class="title">赛事规则</span>
+          <div class="card_des_date">
+            <span> • 比赛采用德州扑克无限注规则。</span>
+            <span> • 每位参赛者将获得相同数量的筹码</span>
+            <span> •  比赛将持续到最后一名选手被淘汰，冠军<br>将获得丰厚奖金和奖杯。</span>
+
+          </div>
+        </div>
+        <div class="card_des">
+          <span class="title">奖品设置</span>
+          <div class="card_des_date">
+            <span> • 冠军：₱100,000现金奖励 + 奖杯</span>
+            <span> • 亚军：₱50,000现金奖励</span>
+            <span> •季军：₱25,000现金奖励</span>
+          </div>
+
+
+        </div>
+        <div class="card_des">
+          <span class="title">报名方式</span>
+          <div class="content">
+            请访问我们的网站或亲临俱乐部前台报名。名额有限，先到先得！<br>
+            准备好迎接挑战了吗？快来展示您的扑克技巧，赢取丰厚奖品吧！
+          </div>
+        </div>
+      </div>
       <n-button>前往</n-button>
     </div>
 
@@ -191,6 +232,10 @@ const tableData = [
   { ranking: 9, user: 'ABC***001', icon: '/img/home/reward.webp', score: '30000', reward: '30,000 VND' },
   { ranking: 10, user: 'ABC***001', icon: '/img/home/reward.webp', score: '30000', reward: '30,000 VND' },
 ]
+
+// const remarkList = [
+// { label: '全部', icon: 'jqdjiconun01', active_icon: 'jqdjiconnn01', id: 1 },
+// ]
 
 
 
@@ -317,7 +362,7 @@ const matchClick = (i: any) => {
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 40px;
+
 }
 
 .remark_img {
@@ -376,10 +421,11 @@ const matchClick = (i: any) => {
       justify-content: center;
       color: #ffffff;
     }
-    .more{
+
+    .more {
       position: absolute;
       right: 0;
-     
+
       width: 90px;
       height: 40px;
       display: flex;
@@ -391,6 +437,7 @@ const matchClick = (i: any) => {
       background-color: #282747;
       cursor: pointer;
       color: #c6c4f5;
+
       &:hover {
         color: #fff;
         background: url("/img/dialog/click.webp?t=@{timestamp}") no-repeat;
@@ -455,13 +502,81 @@ const matchClick = (i: any) => {
   background-color: #0B0B0B;
   border-radius: 16px;
   padding: 16px 30px;
-  .colect_wrap_item_des{
+
+  .colect_wrap_item_des {
     margin-bottom: 15px;
-    div{
+
+    div {
       display: flex;
       justify-content: space-between;
       color: #FFFFFF;
     }
+  }
+}
+
+.card_des_wrap {
+  display: flex;
+  margin-top: 46px;
+  margin-bottom: 30px;
+
+}
+
+.card_des {
+  width: 290px;
+  height: 172px;
+  color: #fff;
+  box-sizing: border-box;
+  position: relative;
+
+  .title {
+    position: absolute;
+    top: 20px;
+    left: 30px;
+  }
+
+  .content {
+    margin-top: 60px;
+    width: 100%;
+    padding: 0 15px;
+    word-break: break-all;
+    font-size: 12px;
+    line-height: 22px;
+    margin-left: 5px;
+  }
+
+  .card_des_date {
+    margin-top: 60px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+    line-height: 22px;
+    margin-left: 15px;
+  }
+
+  &:nth-child(1) {
+    background: url("/img/home/match_card1.webp?t=@{timestamp}") no-repeat;
+    background-size: 103% 100%;
+  }
+
+  &:nth-child(2) {
+    background: url("/img/home/match_card2.webp?t=@{timestamp}") no-repeat;
+    background-size: 100% 100%;
+  }
+
+  &:nth-child(3) {
+    background: url("/img/home/match_card3.webp?t=@{timestamp}") no-repeat;
+    background-size: 100% 100%;
+  }
+
+  &:nth-child(4) {
+    background: url("/img/home/match_card4.webp?t=@{timestamp}") no-repeat;
+    background-size: 100% 100%;
+  }
+
+  &:nth-child(5) {
+    background: url("/img/home/match_card5.webp?t=@{timestamp}") no-repeat;
+    background-size: 100% 100%;
   }
 }
 </style>
