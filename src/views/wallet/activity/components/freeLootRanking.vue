7608a58e14da848387221b740c6392f9842c403b
owen <owen@qq.com>
2024-10-23 01:43:58 +0800

    免费夺宝

---
+++ src/views/wallet/activity/components/freeLootRanking.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="freeLootRanking">
+    <div class="theFirst">
+      <div class="info">
+        <div class="avatar">
+          <Imgt :src="freeLootRankingData.avatar" />
+        </div>
+        <div class="accountNumber">
+          <p>账号：{{ freeLootRankingData.accountNumber }}</p>
+          <p>今日排名：{{ freeLootRankingData.ranking }}</p>
+        </div>
+
+      </div>
+      <div class="gold">
+        <p>试玩金币</p>
+        <div class="goldNumber">
+          <Imgt src="/img/wallet/gold.svg" />
+          <p>{{ freeLootRankingData.gold }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="goldTab">
+      <div class="header">
+        <Imgt src="/img/wallet/goldRanking.svg" class="pointer" />
+      </div>
+
+      <div class="table">
+        <n-data-table striped :bordered="false"  :columns="freeLootRankingData.tableHeader" :data="freeLootRankingData.list">
+          <template #empty>
+            <div class="nodata">
+              <Imgt src="/img/wallet/nodata.webp" alt="nodata" />
+              <div>{{ t('home_page_nomore_data') }}</div>
+            </div>
+          </template>
+        </n-data-table>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+import { reactive, ref, h } from 'vue';
+import Imgt from '@/components/Imgt.vue';
+import { PlatformValueMap } from '@/enums/walletEnum.ts';
+import { convertObjectToDateString } from '@/utils/dateTime.ts';
+
+
+const loading = ref(false);
+
+const freeLootRankingData = reactive({
+  avatar: '/img/header/avatar.webp',
+  accountNumber: 'Kkktey09120',
+  ranking: '1000+',
+  gold: '991,000.00',
+  list: [
+    { avatar: '1', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '2', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '3', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '4', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '5', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '6', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '7', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '8', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '9', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+    { avatar: '10', img: '', name: '六字昵称星星', gold: '试玩金币：100W', award: '奖励：3,000' },
+  ],
+  tableHeader: [
+    { title: '排名', key: 'avatar' },
+    { title: '头像', key: 'img' },
+    { title: '昵称', key: 'name' },
+    { title: '试玩金币', key: 'gold' },
+    { title: '本期奖励', key: 'award' },
+  ],
+});
+
+
+</script>
+
+<style scoped lang="less">
+.freeLootRanking {
+  padding-top: 20px;
+
+  .theFirst {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 96px;
+    padding: 0 30px;
+    background: linear-gradient(94.51deg, #FA397E 0%, #FF852D 99.32%);
+    border-radius: 12px;
+
+    p {
+      margin: 0;
+    }
+
+    .info {
+      display: flex;
+      align-items: center;
+
+      .avatar {
+        margin-right: 10px;
+
+        > img {
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
+        }
+      }
+
+      .accountNumber {
+        font-size: 18px;
+
+        > p:nth-of-type(2) {
+          font-size: 14px;
+        }
+      }
+    }
+
+    .gold {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      > p {
+        font-size: 18px;
+        font-family: PingFangSC;
+        margin-bottom: 4px;
+      }
+
+      .goldNumber {
+        display: flex;
+        align-items: center;
+        border-radius: 12px;
+        padding-right: 6px;
+        background-color: rgba(0, 0, 0, 0.3);
+
+        img {
+          width: 18px;
+          height: 18px;
+        }
+
+        p {
+          font-size: 14px;
+          font-family: PingFangSC;
+        }
+      }
+    }
+
+
+  }
+
+  .goldTab {
+    margin-top: 25px;
+    background-color: rgba(23, 27, 68, 1);
+    border: 1px solid rgba(38, 41, 76, 1);
+    border-radius: 8px;
+
+    .header {
+      display: flex;
+      justify-content: center;
+
+      > img {
+        width: 214px;
+        height: 32px;
+        margin-top: -6px;
+      }
+    }
+
+    .table {
+      padding: 25px 20px 0;
+      :deep .n-data-table-thead > tr > th {
+        color: rgba(175, 186, 189, 1);
+        border-bottom: rgba(38, 41, 76, 1);
+        background-color: rgba(27, 31, 75, 1) !important;
+        //border: 1px solid rgba(38, 41, 76, 1) !important;
+      }
+      :deep .n-data-table .n-data-table-td{
+        color: rgba(175, 186, 189, 1) !important;
+        border-bottom: rgba(38, 41, 76, 1);
+        background-color: rgba(18, 19, 54, 1) !important;
+      }
+      :deep .n-data-table .n-data-table-tr.n-data-table-tr--striped .n-data-table-td{
+        background-color: rgba(13, 14, 46, 1) !important;
+      }
+
+    }
+  }
+}
+</style>
--- src/views/wallet/activity/index.vue
+++ src/views/wallet/activity/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="activity">
     <div class="tab">
-      <span :class="state.active == i ? 'active' : ''" v-for="(_game, g, i) in activityTitleList" :key="i"
-        @click="changeTab(i)">{{ t(g) }}</span>
+      <span :class="state.active == i ? 'active' : ''" v-for="(_game, g, i) in activityTitleList"
+            :key="i"
+            @click="changeTab(i)">{{ t(g) }}</span>
     </div>
     <div class="img_box">
-      <div v-for="item in homeActivityList" :key="item.id">
+      <div v-for="item in homeActivityList" :key="item.id" @click="defineModel(item)">
         <img :src="item.pic_link" alt="" />
         <p>
           <i>{{ item.name }}</i>
@@ -15,10 +16,46 @@
         </p>
       </div>
     </div>
+    <!--    <n-modal v-model:show="state.showModal">-->
+    <!--      <n-card :title="t('activity_page_detail')" closable class="avatar_set" @close="state.showModal = false" :bordered="false" size="huge"-->
+    <!--        role="dialog" aria-modal="true">-->
+    <!--&lt;!&ndash;        <Imgt v-if="state.detailImg" :src="t(state.detailImg)" />&ndash;&gt;-->
+    <!--        <FreeLoot/>-->
+    <!--      </n-card>-->
+    <!--    </n-modal>-->
+
+
     <n-modal v-model:show="state.showModal">
-      <n-card :title="t('activity_page_detail')" closable @close="state.showModal = false" :bordered="false" size="huge"
-        role="dialog" aria-modal="true">
-        <Imgt v-if="state.detailImg" :src="t(state.detailImg)" />
+      <n-card class="avatar_set" @close="state.showModal = false" :bordered="false" size="huge"
+              role="dialog" aria-modal="true">
+        <!--        <Imgt v-if="state.detailImg" :src="t(state.detailImg)" />-->
+        <!--        <FreeLoot v-model="state.showModal"/>-->
+
+        <div class="freeLoot main_setting">
+          <h4 class="top_title">
+            <span>{{ t('免费夺宝') }}</span>
+            <i>
+              <iconpark-icon
+                @click="state.showModal = false"
+                icon-id="tanctongyguanb"
+                color="#fff"
+                size="1.2rem"
+              ></iconpark-icon>
+            </i>
+          </h4>
+
+
+          <div class="main_body">
+            <div class="tab">
+            <span :class="state.freeLootActive == i ? 'active' : ''" v-for="(item, i) in state.freeLootTab"
+            :key="i"
+            @click="changeFreeLootTab(item, i)">{{ t(item.name) }}</span>
+            </div>
+            <div class="freeComponent">
+              <component :is="state.freeLootComponent"></component>
+            </div>
+          </div>
+        </div>
       </n-card>
     </n-modal>
   </div>
@@ -28,17 +65,19 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, reactive } from "vue";
-import Imgt from "@/components/Imgt.vue";
+import { onMounted, onUnmounted, reactive } from 'vue';
 // import { useRoute } from "vue-router";
-import { useI18n } from "vue-i18n";
-import { Page } from "@/store/page";
-import pinia from "@/store/index";
-import { storeToRefs } from "pinia";
-import { NetPacket } from "@/netBase/NetPacket";
-import { Net } from "@/net/Net";
-import { MessageEvent2 } from "@/net/MessageEvent2";
-import { NetMsgType } from "@/netBase/NetMsgType";
+import { useI18n } from 'vue-i18n';
+import { Page } from '@/store/page';
+import pinia from '@/store/index';
+import { storeToRefs } from 'pinia';
+import { NetPacket } from '@/netBase/NetPacket';
+import { Net } from '@/net/Net';
+import { MessageEvent2 } from '@/net/MessageEvent2';
+import { NetMsgType } from '@/netBase/NetMsgType';
+import FreeLoot from '@/views/wallet/activity/components/freeLoot.vue';
+import FreeLootRanking from '@/views/wallet/activity/components/freeLootRanking.vue';
+import FreeLootRule from '@/views/wallet/activity/components/freeLootRule.vue';
 // import Calendar from '@/components/Calendar.vue'
 
 const { activityTitleList, homeActivityList } = storeToRefs(Page(pinia));
@@ -73,20 +112,29 @@ const changeTab = (tabId: number) => {
 
 const state: any = reactive({
   active: 0,
-  name: "home_page_all",
-  link: "",
+  name: 'home_page_all',
+  link: '',
   showModal: false,
   detailImg: null,
   arr: [
     {
-      date: "2018/8/1",
-      className: "mark1",
+      date: '2018/8/1',
+      className: 'mark1',
     },
     {
-      date: "2018/8/13",
-      className: "mark2",
+      date: '2018/8/13',
+      className: 'mark2',
     },
   ],
+  freeLootActive: 0,
+  freeLootComponent: FreeLoot,
+  freeLootTab: [
+    { name: '免费夺宝', component: FreeLoot },
+    { name: '夺宝排行榜', component: FreeLootRanking },
+    { name: '规则说明', component: FreeLootRule }
+    // { name: '夺宝排行榜', component: 'freeLootRanking' },
+    // { name: '规则说明', component: 'freeLootRule' }
+  ]
 });
 // const popDetail = (item: any) => {
 //   state.detailImg = null;
@@ -145,6 +193,22 @@ const state: any = reactive({
 const handleActivetys = async (res: any) => {
   await Page(pinia).setActivityTitleList(res.promo);
 };
+
+
+// 点击按钮弹窗
+const defineModel = (item: any) => {
+  console.log(item.name, '--item--');
+  state.showModal = true;
+};
+
+
+const changeFreeLootTab = (item: any, tabId: number) => {
+  console.log(item, 'changeFreeLootTab');
+  state.freeLootActive = tabId;
+  state.freeLootComponent = item.component;
+}
+
+
 onMounted(() => {
   // state.name = route.query.typeName
   // 获取所有活动  activities_category_icon_
@@ -159,6 +223,61 @@ onUnmounted(() => {
 </script>
 
 <style lang="less" scoped>
+.avatar_set {
+  :deep(.n-card__content) {
+    padding: 0;
+  }
+
+  .main_setting {
+    width: 852px;
+    border-radius: 14px;
+    border: solid 1.4px #322c59;
+    overflow: hidden;
+
+    .top_title {
+      margin: 0;
+      width: 100%;
+      position: relative;
+      height: 60px;
+      line-height: 60px;
+      font-size: 16px;
+      color: #fff;
+      border-top-left-radius: 14px;
+      border-top-right-radius: 14px;
+      text-align: center;
+      border-bottom: 1px solid #26294c;
+      background-color: #14173a;
+
+      > i {
+        position: absolute;
+        top: 5px;
+        right: 15px;
+        cursor: pointer;
+      }
+    }
+
+    .main_body {
+      //min-height: 900px;
+      padding: 40px 40px;
+      > .tab {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 44px;
+        > span {
+          display: block;
+          width: 33%;
+          font-size: 16px;
+        }
+      }
+      >.freeComponent {
+        height: 100%;
+      }
+    }
+  }
+
+}
+
 .activity {
   border-radius: 16px;
   background: linear-gradient(350deg, #090b32 7.62%, #0d1042 92.38%);
@@ -183,7 +302,7 @@ onUnmounted(() => {
     margin-top: 20px;
     justify-content: space-between;
 
-    >div {
+    > div {
       width: 500px;
       height: 202px;
       border-radius: 12px;
@@ -192,11 +311,11 @@ onUnmounted(() => {
       flex-direction: column;
       margin-bottom: 15px;
 
-      >img {
+      > img {
         height: 160px;
       }
 
-      >p {
+      > p {
         padding: 10px 16px;
         display: flex;
         justify-content: space-between;
@@ -204,12 +323,12 @@ onUnmounted(() => {
         font-size: 16px;
         align-items: center;
 
-        >i {
+        > i {
           font-style: normal;
           color: #afb6bd;
         }
 
-        >span {
+        > span {
           cursor: pointer;
           color: #fff;
           height: 26px;
@@ -219,10 +338,10 @@ onUnmounted(() => {
           display: flex;
           align-items: center;
           background: linear-gradient(180deg, #f7dc82 0%, #ca8528 100%),
-            linear-gradient(180deg, #f7dc82 0%, #a76712 100%);
+          linear-gradient(180deg, #f7dc82 0%, #a76712 100%);
           box-shadow: 0.5px 0.5px 1px 0px #ffeab2 inset;
 
-          >.icon {
+          > .icon {
             margin-left: 2px;
             cursor: pointer;
           }
