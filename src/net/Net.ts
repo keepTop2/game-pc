71395692b93eb836a7c9ada8e07593912d23f1b1
jason <jason@gmail.com>
2024-10-26 17:56:12 +0800

    增加过去标签的协议

--- src/net/Net.ts
+++ src/net/Net.ts
@@ -27,16 +27,16 @@ export const getLocale = async () => { // Page(pinia).$state.settings
 export const getSetting = async () => {
     const settingsRes = await fetch('http://18.167.187.79/config_h5_develop.json?' + new Date().getTime())
     const settings = await settingsRes.json()
- 
-    const lang_list_label:any = {
-        en:'home_page_en',
-        zh:'home_page_zh',
-        vn:'home_page_vi',
+
+    const lang_list_label: any = {
+        en: 'home_page_en',
+        zh: 'home_page_zh',
+        vn: 'home_page_vi',
     }
-    settings.lang_list.forEach((item:any) => {
+    settings.lang_list.forEach((item: any) => {
         item.label = lang_list_label[item.name]
         item.value = item.name
-        
+
     });
 
 
